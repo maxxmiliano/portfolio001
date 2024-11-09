1d9be48198fc664cbd84e078de57ab86aa6952c7
unknown <maxmilianossouza@hotmail.com>
2024-11-08 23:25:22 -0300

    termino do portifolio

--- src/script.js
+++ src/script.js
@@ -65,3 +65,17 @@ darkModeIcon.onclick = () => {
   darkModeIcon.classList.toggle('bx-sun');
   document.body.classList.toggle('dark-mode');
 };
+
+/*------------- scroll reveal ---------*/
+ScrollReveal({ 
+
+  //reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });  
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });  
+ScrollReveal().reveal('.home-content h3, .home-content p .about-content', { origin: 'right' });  
